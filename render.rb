0c74e35780af7c260cac1e731ce41234c7f4481e
Lars Christensen <larsch@belunktum.dk>
2016-08-07 09:10:14 +0200

    Add render example

--- render.rb
+++ render.rb
@@ -3,6 +3,11 @@ require 'fileutils'
 
 Dir.glob("*.scad") do |scadname|
   next if scadname == "box.scad"
-  pngname = File.basename(scadname, '.scad') + '.png'
-  system "openscad", "-o", pngname, scadname
+  if scadname =~ /_cut\.scad$/
+    svgname = File.basename(scadname, '.scad') + '.svg'
+    system "openscad", "--render", "-o", svgname, scadname
+  else
+    pngname = File.basename(scadname, '.scad') + '.png'
+    system "openscad", "-o", pngname, scadname
+  end
 end
