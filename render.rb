172f855afd99bf819f93ebe4b9458829995a013b
Lars Christensen <larsch@belunktum.dk>
2016-08-07 09:14:06 +0200

    Convert render to png

--- render.rb
+++ render.rb
@@ -3,11 +3,12 @@ require 'fileutils'
 
 Dir.glob("*.scad") do |scadname|
   next if scadname == "box.scad"
+  pngname = File.basename(scadname, '.scad') + '.png'
   if scadname =~ /_cut\.scad$/
     svgname = File.basename(scadname, '.scad') + '.svg'
     system "openscad", "--render", "-o", svgname, scadname
+    system "convert", svgname, pngname
   else
-    pngname = File.basename(scadname, '.scad') + '.png'
     system "openscad", "-o", pngname, scadname
   end
 end
